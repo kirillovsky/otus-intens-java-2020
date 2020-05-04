864e9c309e346ebcc7616c6c3a6643e851ad9f5f
kirillovsky <kirillow.aleks@gmail.com>
2020-05-04 20:28:49 +0300

    add startup delay for profile

--- OptiHomework/src/main/java/twitter/TweeterReader.java
+++ OptiHomework/src/main/java/twitter/TweeterReader.java
@@ -40,6 +40,7 @@ public class TweeterReader {
     }
 
     public static void main(String[] args) throws Exception {
+        Thread.sleep(10000);
         TweeterReader reader = new TweeterReader();
         reader.run();
     }
